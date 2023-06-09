d817aadfdf32c7b4445aa008cd7d1f7bbd4f13f5
nellaadrs <111825727+nellaadrs@users.noreply.github.com>
2023-06-10 05:26:36 +0700

    Update index.py

--- index.py
+++ index.py
@@ -21,8 +21,9 @@ def load_dataset():
 	return dataset
 
 st.title('Sistem Pendeteksi Anemia')
-st.subheader("R. Bella Aprilia Damayanti	200411100082"),
-("Nella Adrisia Hartono		200411100107"),("Machine Learning B")
+st.subheader("R. Bella Aprilia Damayanti	200411100082")
+st.subheader("Nella Adrisia Hartono		200411100107")
+st.subheader("Machine Learning B")
 
 deskripsi, dataset, modelling, implementasi = st.tabs(["Info", "Dataset", "Modelling", "Implementasi"])
 
